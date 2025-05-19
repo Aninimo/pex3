ef6201aa4875f8f5cf1d0b6c4443b6343784975a
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2025-05-19 19:59:17 -0300

    Create page.tsx

---
+++ src/app/causesOfHunger/page.tsx
@@ -0,0 +1,36 @@
+export default function causesOfHunger(){
+    return(
+        <div className="p-8">
+          <h1 className="text-3xl mb-4">Combate a fome</h1>
+          <p className="w-3/5">A fome é uma das maiores injustiças do mundo moderno. Milhões de pessoas sofrem com a falta de acesso a alimentos nutritivos, levando a consequências devastadoras para a saúde, o bem-estar e a dignidade humana.</p>
+          <br/>
+          <p className="w-3/5">No entanto, a fome não é apenas um problema de falta de alimentos. É também um reflexo de desigualdades sociais, econômicas e políticas que perpetuam a pobreza e a exclusão. É um desafio complexo que requer uma abordagem multifacetada e colaborativa.</p>
+          <h4 className="font-bold mt-4">Causas da Fome</h4>
+          <p>A fome é causada por uma combinação de fatores, incluindo:</p>
+          <ul className="mt-4">
+            <li className="mb-2">• Pobreza e desigualdade econômica</li>
+            <li className="mb-2">• Falta de acesso a terra, água e recursos naturais</li>
+            <li className="mb-2">• Conflitos e crises humanitárias</li>
+            <li className="mb-2">• Mudanças climáticas e degradação ambiental</li>
+            <li>Sistemas alimentares injustos e ineficientes</li>
+          </ul>
+          <h4 className="font-bold mt-4 mb-2">Soluções para o Combate à Fome</h4>
+          <p>Para combater a fome, é necessário adotar uma abordagem integrada que abranja as seguintes áreas:</p>
+          <ul className="mt-2 mb-4">
+            <li className="w-3/5 mb-2"><p><strong>Agricultura Sustentável:</strong>Promover práticas agrícolas sustentáveis, como a agroecologia, para aumentar a produtividade e a resiliência dos sistemas alimentares.</p></li>
+            <li className="w-3/5 mb-2"><p><strong>Acesso a Alimentos:</strong>Garantir o acesso a alimentos nutritivos e seguros, especialmente para as populações mais vulneráveis.</p></li>
+            <li className="w-3/5 mb-2"><p><strong>Desenvolvimento Econômico:</strong>Promover o desenvolvimento econômico inclusivo e sustentável, com foco na criação de empregos e oportunidades de renda.</p></li>
+            <li className="w-3/5 mb-2"><p><strong>Educação e Conscientização:</strong>Educar e conscientizar as comunidades sobre a importância da segurança alimentar e nutricional.</p></li>
+            <li className="w-3/5"><p><strong>Cooperação Internacional:</strong></p>Fortalecer a cooperação internacional para compartilhar conhecimentos, recursos e experiências no combate à fome.</li>
+          </ul>
+          <h4 className="font-bold mt-4">Ação Conjunta</h4>
+          <p className="w-3/5">O combate à fome é um desafio que requer ação conjunta de governos, organizações internacionais, sociedade civil, setor privado e comunidades locais. Juntos, podemos:</p>
+          <ul className="mt-4 mb-4">
+            <li><p><strong>Promover a segurança alimentar:</strong>Garantir o acesso a alimentos nutritivos e seguros para todos.</p></li>
+            <li><p><strong>Erradicar a pobreza:</strong>Promover o desenvolvimento econômico inclusivo e sustentável.</p></li>
+            <li><p><strong>Proteger o meio ambiente:</strong>reservar os recursos naturais e promover práticas sustentáveis.</p></li>
+          </ul>
+          <p className="w-3/5">O combate à fome é um desafio complexo, mas não é impossível. Com ação conjunta, determinação e compromisso, podemos criar um mundo onde todos tenham acesso a alimentos nutritivos e seguros. Vamos juntos lutar pela erradicação da fome e pela dignidade humana!</p>
+        </div>
+    )
+}
