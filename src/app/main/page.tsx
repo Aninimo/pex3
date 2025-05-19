dd3ec0dcc598302dee4707033935e9cff4bd6a55
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2025-05-19 19:46:05 -0300

    Create page.tsx

---
+++ src/app/main/page.tsx
@@ -0,0 +1,44 @@
+export default function Sustainable(){
+    return(
+        <div className="p-8">
+            <h1 className="text-3xl mb-4">Dicas Sustentáveis</h1>
+            <p>Aqui estão algumas dicas sustentáveis para reduzir o desperdício de alimentos, plantar em casa e praticar a compostagem:</p>
+            <h4 className="text-3xl mt-8 mb-4">Reduzindo o Desperdício de Alimentos</h4>
+            <ul>
+                <li className="mb-2"><p><strong>Planeje as refeições:</strong>Antes de ir ao supermercado, planeje as refeições da semana para evitar comprar alimentos que não serão consumidos.</p></li>
+                <li className="mb-2"><p><strong>Faça uma lista de compras:</strong>Anote os alimentos que você precisa comprar e evite comprar impulsivamente.</p></li>
+                <li className="mb-2"><p><strong>Armazene alimentos corretamente:</strong>Aprenda a armazenar alimentos de forma correta para prolongar sua vida útil.</p></li>         
+                <li className="mb-2"><p><strong>Use alimentos próximos ao vencimento:</strong>Priorize o uso de alimentos que estão próximos ao vencimento.</p></li>
+                <li className="mb-2"><p><strong>Congele alimentos:</strong>Congele alimentos que não serão consumidos imediatamente, como frutas, legumes e carnes.</p></li>
+            </ul>
+            <h4 className="text-3xl mt-8 mb-4">Plantando em casa</h4>
+            <ul>
+              <li className="mb-2"><p><strong>Escolha plantas fáceis de cultivar:</strong>Comece com plantas fáceis de cultivar, como alface, tomate e pimentão.</p></li>
+              <li className="mb-2"><p><strong>Use recipientes reciclados:</strong>Use recipientes reciclados, como garrafas de plástico e caixas de papelão, para plantar.</p></li>
+              <li className="mb-2"><p><strong>Mantenha o solo úmido:</strong>Mantenha o solo úmido, mas não encharcado, para evitar a perda de nutrientes.</p></li>
+              <li className="mb-2"><p><strong>Forneça luz solar adequada:</strong>Forneça luz solar adequada para as plantas, dependendo do tipo de planta.</p></li>
+              <li className="mb-2"><p><strong>Aprenda a identificar pragas:</strong>5. Aprenda a identificar pragas comuns e use métodos orgânicos para controlá-las.</p></li>
+            </ul>
+
+            <h4 className="text-3xl mt-8 mb-4">Compostagem</h4>
+            <p className="w-3/5 mb-8">A compostagem é um processo natural e sustentável que transforma resíduos orgânicos em adubo rico em nutrientes, que pode ser utilizado para fertilizar o solo de jardins, hortas e outros espaços verdes. É uma forma de reciclagem do lixo orgânico, contribuindo para a redução do desperdício e para a preservação do meio ambiente.</p>
+            <h4 className="text-3xl mt-8 mb-8">Como funciona a compostagem:</h4>
+            <ul>
+              <li className="mb-2"><p><strong>Separe os resíduos:</strong>Separe os resíduos orgânicos, como restos de comida e folhas, dos resíduos inorgânicos.</p></li>
+              <li className="mb-2"><p><strong>Crie um local de compostagem:</strong>Crie um local de compostagem, como um canto do jardim ou um recipiente específico.</p></li>
+              <li className="mb-2"><p><strong>Adicione materiais:</strong>Adicione materiais, como palha, folhas e restos de comida, ao local de compostagem.</p></li>
+              <li className="mb-2"><p><strong>Mantenha o local úmido:</strong>Mantenha o local de compostagem úmido, mas não encharcado.</p></li>
+              <li><p><strong>Aprenda a usar o composto:</strong>Aprenda a usar o composto resultante como adubo para as plantas.</p></li>
+            </ul>
+
+            <h4 className="text-3xl mt-8 mb-4">Outras dicas suatentáveis</h4>
+            <ul>
+                <li className="mb-2"><p><strong>Use água de forma eficiente:</strong>Use água de forma eficiente, evitando desperdício e usando dispositivos de economia de água.</p></li>
+                <li className="mb-2"><p><strong>Reduza o uso de plástico:</strong>Reduza o uso de plástico, optando por alternativas, como sacolas de tecido e recipientes de vidro.</p></li>
+                <li className="mb-2"><p><strong>Mantenha o solo úmido:</strong>Mantenha o solo úmido, mas não encharcado, para evitar a perda de nutrientes.</p></li>
+                <li className="mb-2"><p><strong>Forneça luz solar adequada:</strong>Forneça luz solar adequada para as plantas, dependendo do tipo de planta.</p></li>
+                <li><p><strong>Aprenda a reduzir resíduos:</strong>Aprenda a reduzir resíduos, evitando o uso de produtos descartáveis e optando por alternativas sustentáveis.</p></li>
+            </ul>
+        </div>
+    )
+}
