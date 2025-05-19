fe515c6292a1d479f77c89ac51d7a361bdbc1a70
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2025-05-19 20:00:46 -0300

    Create page.tsx

---
+++ src/app/technology/page.tsx
@@ -0,0 +1,41 @@
+export default function Tecnology(){
+    return(
+      <div className="p-8"> 
+        <h1 className="text-3xl mb-8">Tecnologia no campo</h1>
+        <p className="w-3/5">A tecnologia no campo é uma área em constante evolução, com muitos benefícios e desafios. Ao abordar esses desafios e oportunidades, podemos garantir que a tecnologia seja usada de forma eficaz e sustentável para melhorar a produtividade, a qualidade e a segurança da agricultura.</p>
+        <div className="mt-4">
+            <p>Algumas das tecnologias mais promissoras que estão sendo aplicadas na agricultura incluem:</p>
+            <ul className="mt-4">
+              <li>
+                <p className="w-3/5"><strong>Agricultura de Precisão:</strong>Utiliza sensores, drones e satélites para coletar dados sobre o solo, o clima e as plantas, permitido que os agricultores tomem decisões mais informadas sobre a aplicação de fertilizantes, pesticidas e água.</p>
+              </li>
+              <li className="mt-8">
+                <p className="w-3/5"><strong>Internet das coisas (IoT):</strong>Conecta dispositivos e sensores para coletar e analisar dados em tempo real, permitindo que os agricultores monitorem e controlem remotamente as condições do campo.</p>
+              </li>
+              <li className="mt-8">
+                <p className="w-3/5"><strong>Inteligência Artificial (IA):</strong>Analisa dados e identifica padrões para prever problemas e otmizar processos, como a detecção de doenças em plantas e a otimização da colheita.</p>
+              </li>
+              <li className="mt-8">
+                <p className="w-3/5"><strong>Biologia Sintética:</strong>Desenvolve novas varirdades de plantas e microorganismos que são mais resistentes a doenças e pragas, e que podem ser cultivados de forma mais sustentável.</p>
+              </li>
+            </ul>
+            <h1 className="text-3xl mt-8">Benefícios da Tecnologia no Campo</h1>
+            <p>A adoção da tecnologia no campo pode trazer muitos benefícios, incluindo:</p>
+            <ul className="mt-4">
+              <li><p className="w-3/5"><strong>Aumento da produtividade:</strong>A tecnologia pode ajudar a otmizar processos e a reduzir custos, permitindo que os agricultores produzam mais com menos recursos.</p></li>
+              <li className="mt-8"><p className="w-3/5"><strong>Redução do Uso de Recursos:</strong>A tecnologia pode ajudar a reduzir o uso de água, fertilizantes e pestícidas, tornando a agricultura mais sustentável.</p></li>
+              <li className="mt-8"><p className="w-3/5"><strong>Melhoria da Qualidade dos Produtos:</strong>A tecnologia pode ajudar a melhorar a qualidade  dos produtos, reduzindo a presença de resíduos químicos e melhorando a segurança alimentar.</p></li>
+              <li className="mt-8"><p className="w-3/5"><strong>Aumento da Eficiência:</strong> A tecnologia pode ajudar a reduzir o tempo e o esforço necessário para realizar tarefas, permitindo que os agricultores se concentrem em outras atividades.</p></li>
+            </ul>
+            <h4 className="text-3xl mt-8 mb-4">Desafios e Oportunidades</h4>
+            <p className="w-3/5">Embora a tecnologia no campo ofereça muitos benefícios, também existem desafios e oportunidades que precisam ser abordados, incluindo:</p>
+            <ul className="mt-8">
+              <li><p className="w-3/5"><strong>Acesso á Tecnologia:</strong>Muitos agricultores, especialmente em áreas rurais, não têm acesso á recnologia necessária para aproveitar os benefícios da agricultura de precisão.</p></li>
+              <li className="mt-4"><p className="w-3/5"><strong>Treinamento e Educação:</strong>Os agricultores precisam de treinamento e educação para usar a tecnologia de forma eficaz.</p></li>
+              <li className="mt-4"><p className="w-3/5"><strong>Privacidade e Segurança:</strong>A coleta e o uso de dados precisam ser feitos de forma segura e respeitosa da privacidade dos agricultores.</p></li>
+              <li className="mt-4"><p className="w-3/5"><strong>Integração com a Agricultura Tradicional:</strong>A tecnologia precisa ser integrada com as práticas agrícolas tradicionais para garantir que os benefícios sejam compartilhados por todos.</p></li>
+            </ul>
+        </div>
+      </div>
+    )
+}
